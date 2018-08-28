f2dff698277135c3670e3994c5d92945970c5bb2
xuxiaojie <2012609543@qq.com>
2018-08-28 17:33:35 +0800

    商品页面左侧导航开发

--- src/components/Goods/Goods.vue
+++ src/components/Goods/Goods.vue
@@ -1,6 +1,51 @@
 <!-- 商品展示  -->
+<style>
+  .goods{
+    display: flex;
+  }
+  .goods-menu{
+    width: 26%;
+    height: auto;
+    /*align-self: stretch;*/
+    background-color: #ece9e9;
+  }
+  .goods-menu ul li{
+    height: 50px;
+    text-align: left;
+    padding-left: 12px;
+    padding-right: 3px;
+    /*line-height: 50px;*/
+    display: flex;
+    align-items: center;
+    /*font-family: "微软雅黑";*/
+    color: #3c3b3b;
+    line-height: 18px;
+    border-bottom: solid 1px #e0d6d6;
+  }
+  .goods-menu ul li:hover{
+    background-color: white;
+  }
+</style>
 <template>
-  <div>商品展示</div>
+  <div class="goods">
+    <div class="goods-menu">
+      <ul>
+        <li>热销榜</li>
+        <li>单人特色套餐</li>
+        <li>特色粥品</li>
+        <li>热销榜</li>
+        <li>单人特色套餐</li>
+        <li>特色粥品</li>
+        <li>热销榜</li>
+        <li>单人特色套餐</li>
+        <li>特色粥品</li>
+        <li>热销榜</li>
+        <li>单人特色套餐</li>
+        <li>特色粥品</li>
+      </ul>
+    </div>
+    <div class="goods-foots"></div>
+  </div>
 
 </template>
 
@@ -8,6 +53,3 @@
 
 </script>
 
-<style>
-
-</style>
