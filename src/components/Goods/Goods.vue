f19f2399c98c4ffdfe9fe777bf8af21b97e91eeb
xuxiaojie <2012609543@qq.com>
2018-08-29 10:24:14 +0800

    商品页面右侧导航开发

--- src/components/Goods/Goods.vue
+++ src/components/Goods/Goods.vue
@@ -7,7 +7,7 @@
     width: 26%;
     height: auto;
     /*align-self: stretch;*/
-    background-color: #ece9e9;
+    background-color: gainsboro;
   }
   .goods-menu ul li{
     height: 50px;
@@ -20,17 +20,20 @@
     /*font-family: "微软雅黑";*/
     color: #3c3b3b;
     line-height: 18px;
-    border-bottom: solid 1px #e0d6d6;
+    border-bottom: solid 1px #d4cbcb;
   }
-  .goods-menu ul li:hover{
+  .goods-menu ul li.active{
     background-color: white;
+  }
+  .goods-foots{
+    background-color: gainsboro;
   }
 </style>
 <template>
   <div class="goods">
     <div class="goods-menu">
       <ul>
-        <li>热销榜</li>
+        <li class="active">热销榜</li>
         <li>单人特色套餐</li>
         <li>特色粥品</li>
         <li>热销榜</li>
@@ -44,12 +47,24 @@
         <li>特色粥品</li>
       </ul>
     </div>
-    <div class="goods-foots"></div>
+    <div class="goods-foots">
+      <vgoods-foots-item></vgoods-foots-item>
+    </div>
   </div>
 
 </template>
 
 <script>
+  import GoodsFootsItem from "@/components/Goods/goods-foots/Goods-foots-item"
+  export default {
+    data:function(){
+      return {
 
+      }
+    },
+    components:{
+      vgoodsFootsItem:GoodsFootsItem
+    }
+  }
 </script>
 
